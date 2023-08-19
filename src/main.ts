a47f86ca4d7e7f7f47c78d8f4be2995c2ffe8ed1
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-08-19 18:43:01 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./styles/reset.css";
+import "./styles/root.css";
+import "./styles/main.css";
+import "./styles/components.css";
+import "./styles/media-queries.css";
+import "./styles/utils.css";
