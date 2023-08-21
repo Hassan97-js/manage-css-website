84e715382907aecf27731a44f81bbafa41e7a519
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-08-21 14:41:51 +0200

    Commits: - create btn component - add more utility classes - start to develop mobile navigation

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,9 @@ import "./styles/main.css";
 import "./styles/components.css";
 import "./styles/utils.css";
 import "./styles/media-queries.css";
+
+const hamburgerButton = document.getElementById("menu-toggler");
+
+hamburgerButton?.addEventListener("click", () => {
+  hamburgerButton.classList.toggle("active");
+});
