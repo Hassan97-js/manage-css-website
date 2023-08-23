f0c121b139eb483c57a413b170c3805ab1eafa15
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-08-23 20:26:23 +0200

    add mobile menu styles (unfinished)

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import "./styles/components.css";
 import "./styles/utils.css";
 import "./styles/media-queries.css";
 
-const hamburgerButton = document.getElementById("menu-toggler");
+const menuTogglerButton = document.getElementById("menu-toggler");
+const primaryNavbarElement = document.body.querySelector(".primary-navbar");
 
-hamburgerButton?.addEventListener("click", () => {
-  hamburgerButton.classList.toggle("active");
+console.log(primaryNavbarElement);
+
+menuTogglerButton?.addEventListener("click", () => {
+  menuTogglerButton.classList.toggle("active");
+  primaryNavbarElement?.classList.toggle("active");
 });
